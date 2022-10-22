59d0513bbc1208393d626b7b9b39d495161e33b0
Melvick19 <melvick.ocanto@gmail.com>
2022-10-22 14:24:20 -0400

    Third commit about JSLearning

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var frutas = ["Manzana", "Cambur", "Mango", "Fresa"];
+
+var masFrutas = frutas.push("Pera");
+
+var borraUltima = frutas.pop("Uvas")
+
+var newFruta = frutas.unshift("Pina")
+
+var deleteFirst = frutas.shift("Pina")
