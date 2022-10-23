a2370038126491d3bf64106e6d9dac186900c35c
Melvick19 <melvick.ocanto@gmail.com>
2022-10-22 21:20:25 -0400

    Arreglo

--- 30cars.js
+++ 30cars.js
@@ -1,17 +1,3 @@
-function auto(marca, modelo, annio) {
-    this.marca = marca;
-    this.modelo = modelo;
-    this.annio = annio;
-}
-
-var newAuto = new auto("Tesla", "Model 3", 2020);
-var newAuto2 = new auto("Hyundai", "Tucson", 2007);
-var newAuto3 = new auto("Ford", "Explorer", 2013);
-
-
-
-
-
 var auto1 = ["Ford", " Explorer", " 2013\n"]
 var auto2 = ["Jeep", " Cherokee", " 2009\n"]
 var auto3 = ["Hyundai", " Tucson", " 2007\n"]
