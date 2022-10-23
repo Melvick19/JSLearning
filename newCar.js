d11db009a414f3d3084f203778de36d2ce46d60b
Melvick19 <melvick.ocanto@gmail.com>
2022-10-22 22:18:27 -0400

    Fin del curso basico de JS Platzi

--- newCar.js
+++ newCar.js
@@ -1,8 +1,16 @@
-var miAuto = {
-    marca: "Toyota",
-    modelo: "Corolla",
-    annio: 2020,
-    detalles: function(){
-        console.log(`Auto${this.modelo} ${this.annio}`);
-    }
-}
+var cars = [
+    { marca: "Jeep", modelo: "Grand Cherokee", annio: 2009},
+    { marca: "Jeep", modelo: "Cherokee", annio: 2010},
+    { marca: "Chevrolet", modelo: "Grand Vittara", annio: 2007},
+    { marca: "Ford", modelo: "Explorer", annio: 2013},
+    { marca: "Hyundai", modelo: "Tucson", annio: 2007},
+    { marca: "Toyota", modelo: "Corolla", annio: 2012},
+    { marca: "Kia", modelo: "Sportage", annio: 2011},
+    { marca: "Mitsubishi", modelo: "Lanser", annio: 2008},
+    { marca: "Toyota", modelo: "4 Runner", annio: 2018},
+    { marca: "Nissan", modelo: "Patrol", annio: 2014},
+];
+
+cars.forEach(function(car){
+    console.log(car.marca + " " + car.modelo + " " + car.annio);
+});
